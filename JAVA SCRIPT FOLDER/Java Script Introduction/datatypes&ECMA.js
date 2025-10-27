dc16fc0a3195252c9cac7931a8ff8d65ca8b256f
Daksh Singhal <psinghal651@gmail.com>
2025-10-28 00:57:04 +0530

    Data types theory and ecma

---
+++ JAVA SCRIPT FOLDER/Java Script Introduction/datatypes&ECMA.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all Js code as newer version/modern version
+
+// alert("Hello") //pop up alert box and used with connection of html in web pages&we re using node.js
+
+console.log(3+3)//code readability should be high
+console.log("Daksh") ; //print in the console
+
+//ECMA is short for European Computer Manufacturers 
+// Association (don’t worry, no one calls it that). 
+// It’s the standards organization that maintains the 
+// ECMAScript specification — the official rulebook for how
+//  JavaScript should behave.
+
+let name="hitesh"
+console.log(typeof name);
+let age=18
+let isloggedin=true
+let num=BigInt(1234567890123456789012345678901234567890n)
+console.log(typeof num);
+let state
+console.log(typeof state);
+let nullvar=null
+console.log(typeof nullvar);
+
+//data types in js
+//number=> 2 to the power 53
+//bigint=>more than 2 to the power 53
+//string==>""
+//boolean=>true/false
+//null==>empty value/standalone value
+//undefined==>value not assigned

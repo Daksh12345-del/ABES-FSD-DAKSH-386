155decd4ad92155c4640e82a070ea9fa03889903
Daksh Singhal <psinghal651@gmail.com>
2025-10-29 00:41:41 +0530

    Data Type

---
+++ JAVA SCRIPT FOLDER/Java Script Introduction/datatype_conversion_confusion.js
@@ -0,0 +1,30 @@
+// let score=undefined //It giges output as NaN
+// let score="33abc"
+// let score=null
+// let score=true
+// console.log(typeof score);
+
+// let valueinnumber=Number(score) //string can be converted lso by writinh String() function
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber); //NaN=>not a number :when we try to convert a non number string to number then it gives the output as NaN
+
+//"33abc"=>NaN
+//"33"=>33
+//true=>1
+//false=>0
+//null=>0
+//undefined=>NaN
+
+// let isloggedin="Daksh"
+// let booleanisloggedin=Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+//1=>True 0=>false
+//"Daksh"=>true.  " "=>false
+//null=>false
+//undefined=>false
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(typeof stringnumber);
+console.log(stringnumber);

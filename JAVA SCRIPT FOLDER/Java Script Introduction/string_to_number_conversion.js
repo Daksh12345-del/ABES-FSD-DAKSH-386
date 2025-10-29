93267a03a088b5b837020033c471fcfeb6e0199d
Daksh Singhal <psinghal651@gmail.com>
2025-10-30 00:49:29 +0530

    String datatype

---
+++ JAVA SCRIPT FOLDER/Java Script Introduction/string_to_number_conversion.js
@@ -0,0 +1,34 @@
+let value=3
+let neg_value=-value
+console.log(neg_value)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="Hello"
+let str2=" Daksh"
+let str3=str1+str2
+console.log(str3)
+
+// console.log("1"+"1")
+// console.log(1+"2");
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3); //operator precedence: */%(left to right) then +-(left to right)
+console.log(true);
+console.log(+true);
+console.log(+"");
+let num1,num2,num3
+num1=num2=num3=2+3
+console.log(num1,num2,num3);
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
+
+//link to study
+//Have to study for prefix and postfix increment and decrement
